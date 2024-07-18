99cdfbbeafe1d5254214546bedc8e6162c293a6d
sg <sg@sg-mac.local>
2024-07-18 14:41:49 -0500

    fix typo

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -6,7 +6,7 @@ import { App } from 'aws-cdk-lib';
 const app = new App();
 new BookStack(app, 'InfraStack', {
   env: {
-    account: process.env.AWS_ACCOUNT_ID,
-    region: process.env.AWS_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
 });
