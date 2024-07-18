5b87e8c3042ad19dc97eed2bc87155d1453281d5
sg <sg@sg-mac.local>
2024-07-18 14:39:33 -0500

    add env for bookstack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,5 +5,8 @@ import { App } from 'aws-cdk-lib';
 
 const app = new App();
 new BookStack(app, 'InfraStack', {
-
+  env: {
+    account: process.env.AWS_ACCOUNT_ID,
+    region: process.env.AWS_REGION,
+  },
 });
